7bc196e80a110c9efdf4e3ff91d67264ac2a514e
manuel blanco <mandresblac@gmail.com>
2024-02-16 21:21:35 -0500

    commit 3

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,24 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
- /* login() {
-  const { value: formValues } = await Swal.fire({
-  title: "Multiple inputs",
-  html: `
-    <input type="email" id="swal-input1" class="swal2-input">
-    <input type="password" id="swal-input2" class="swal2-input">
-  `,
-  focusConfirm: false,
-  preConfirm: () => {
-    return [
-      document.getElementById("swal-input1").value,
-      document.getElementById("swal-input2").value
-    ];
+ async login() {
+    const { value: formValues } = await Swal.fire({
+    title: "Multiple inputs",
+    html: `
+      <input type="email" id="swal-input1" class="swal2-input">
+      <input type="password" id="swal-input2" class="swal2-input">
+    `,
+    focusConfirm: false,
+    preConfirm: () => {
+      return [
+        (document.getElementById('identificador') as HTMLInputElement).value,
+        (document.getElementById('identificador') as HTMLInputElement).value,
+      ];
+    }
+  });
+  if (formValues) {
+    Swal.fire(JSON.stringify(formValues));
+    console.log(formValues)
   }
-});
-if (formValues) {
-  Swal.fire(JSON.stringify(formValues));
-  console.log(formValues)
-}
- } */
+ }
 }
