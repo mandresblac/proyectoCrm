85cc79748e36d7290ee9eae88b872983bbf407e8
manuel blanco <mandresblac@gmail.com>
2024-02-17 20:36:54 -0500

    commit 4

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SignUpComponent } from '../../components/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [SignUpComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
